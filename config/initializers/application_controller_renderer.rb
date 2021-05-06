0d8d6df09c5eb8c01993f41f062e19c78a33f2d2
going2631 <tomo.moedora1212@gmail.com>
2021-05-06 06:08:54 +0000

    [Finish] 6-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e67ad8c0e5c10c92729785bec661f15c6addc9837d9954606b7314716286c3e6fc0c8a40dc1dd0ff818e022382d5b3d56a214333dfd8da340dd101419ce41987'
